1597c526f1e08f8c225b3f97d0c1706b6027e143
Shiuli Das <76279101+Shiuli1821@users.noreply.github.com>
2021-08-17 19:22:26 +0530

    Add files via upload

---
+++ Playlist.py
@@ -0,0 +1,15 @@
+class PlaylistQueue:
+
+    queue = None
+
+    def __init__(self):
+        self.queue = []
+
+    # This method appends songs to the Playlist queue
+    def append(self, url):
+        """Write your code here to append the song to queue."""
+
+    # This method pops the song from Playlist queue
+    def pop(self):
+        """Write your code here to pop the song from queue."""
+
---
+++ Songs_backStack.py
@@ -0,0 +1,14 @@
+class Songs_BackStack:
+
+    stack = None
+
+    def __init__(self):
+        self.stack = []
+
+    # This method appends songs to the backstack
+    def append(self, song):
+        """Write your code here to append songs to backstack"""
+
+    # This method pops the song from backstack.
+    def pop(self):
+        """Write your code here to pop the song from the stack."""
